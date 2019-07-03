a6a5e29abb5ffa66971feeea8c54ae88d14d5af3
Josias Iquabius <iquabius@gmail.com>
2019-07-02 22:01:23 -0400

    Add text-based battery widget

--- rc.lua
+++ rc.lua
@@ -234,6 +234,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            require("deps/battery-widget") {},
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
